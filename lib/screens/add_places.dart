b6d7c1b289a03daa8ee36174e34a372aad4e2ca7
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-11-30 19:12:18 +0530

    pick an image input

--- lib/screens/add_places.dart
+++ lib/screens/add_places.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -51,6 +52,9 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+            const SizedBox(height: 16),
+//Image Input
+            ImageInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
