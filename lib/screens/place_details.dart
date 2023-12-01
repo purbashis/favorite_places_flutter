4c9b27fc33ae1854f744f6e587690998adfa15e9
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-12-01 11:22:00 +0530

    previewing the picked image by using stack

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -7,14 +7,21 @@ class PlaceDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
+      appBar: AppBar(
         title: Text(place.title),
       ),
-      body: Center(
-child:  Text(place.title,style: Theme.of(context).textTheme.titleMedium!.copyWith(
-            color: Theme.of(context).colorScheme.onBackground,),
-),),
+      body: 
+      Stack(
+        children: [
+          Image.file(place.image,
+           fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+            
+            ),
+         
+        ],
+      )
     );
-
   }
 }
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -23,8 +23,11 @@ class PlacesListScreen extends ConsumerWidget {
               icon: const Icon(Icons.add)),
         ],
       ),
-      body:  PlacesList(
-        places: userPlaces,
+      body:  Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: PlacesList(
+          places: userPlaces,
+        ),
       ),
     );
   }
