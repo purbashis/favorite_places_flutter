c913dda72628d82f68a9d6a7a3a5d7f349ffa67b
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-12-27 10:56:42 +0530

    storing the picked image locally

--- lib/provider/user_places.dart
+++ lib/provider/user_places.dart
@@ -24,27 +24,29 @@
 //     (ref) => UserPlacesNotifier(),
 //   );
 
-
 //   // void removePlace(String placeId) {
 //   //   state = state.where((place) => place.id != placeId).toList();
 //   // }
 // }
 import 'dart:io';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:favorite_places/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title , File image , PlaceLocation location) {
-    final newPlace = Place(title: title ,image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await  syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+   final copiedImage = await  image.copy('${appDir.path}/$filename');
+
+    final newPlace = Place(title: title, image: copiedImage, location: location);
     state = [newPlace, ...state];
   }
 }
-
-
 
 final userPlacesProvider =
     StateNotifierProvider<UserPlacesNotifier, List<Place>>(
